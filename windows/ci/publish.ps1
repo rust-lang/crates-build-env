82abbdfe258b2944d30b5c5b405ce10749f78974
Dylan MacKenzie <ecstaticmorse@gmail.com>
2019-02-20 13:53:03 +0100

    Ensure "DOCKER_{PASSWORD,USERNAME}" are set

--- windows/ci/publish.ps1
+++ windows/ci/publish.ps1
@@ -3,6 +3,13 @@ $ErrorActionPreference = "Stop"
 # Build the image
 ./windows/ci/build.ps1
 
+foreach ($var in "DOCKER_USERNAME", "DOCKER_PASSWORD") {
+    if (-not (Test-Path "env:$var")) {
+        echo "Environment variable \"$var\" not set"
+        exit 22
+    }
+}
+
 # Log in to dockerhub
 [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Env:DOCKER_PASSWORD)).Trim() `
     | docker login --username "$Env:DOCKER_USERNAME" --password-stdin
