b9f9b09bc1fcf50e7c5d4dd1da56765153306b64
Pietro Albini <pietro@pietroalbini.org>
2019-09-16 11:00:33 +0200

    update the linux build environment to reflect docs.rs

--- ci/lint.sh
+++ ci/lint.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Ensure packages lists are sorted
-for file in packages packages-backports; do
+for file in packages; do
     cat "${file}.txt" | sort > "/tmp/sorted-${file}.txt"
     if ! diff "/tmp/sorted-${file}.txt" "${file}.txt" >/dev/null 2>&1; then
         echo "Lint error: ${file}.txt is not sorted"
